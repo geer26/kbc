ff3deaf66520d287dd7617b2efd3ea253493d6fa
geer26 <gergo.kurucz@gmail.com>
2023-02-14 14:20:48 +0100

    v 0.1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Loginmodal msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Loginmodal from './components/Loginmodal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Loginmodal
   }
 }
 </script>
